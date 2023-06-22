76f9337f41dd274868b2178d0e6e8fdee37c18be
MACBOOK PRO <macbookpro@Sebastine.local>
2023-06-22 22:51:59 +0100

    connecting to the database

--- src/student/management/conn/mySqlConnection.java
+++ src/student/management/conn/mySqlConnection.java
@@ -1,4 +1,22 @@
 package student.management.conn;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class mySqlConnection {
+
+    Connection conn = null;
+
+    public static Connection getConnection() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/");
+            JOptionPane.showMessageDialog(null, "Connection Established");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            return null;
+        }
+    }
 }
